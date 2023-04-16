846efbe221064580e3b1299356411fb7f550dbb3
MysteryMooCows <rod.j.myers@gmail.com>
2023-04-16 00:04:57 -0400

    Created config.py, check for OpenAI key using dotenv, created dotenv template, updated requirements.txt

---
+++ src/config.py
@@ -0,0 +1,34 @@
+import abc
+import os
+from dotenv import load_dotenv
+
+
+# Load environment variables from .env file
+load_dotenv()
+
+
+class Singleton(abc.ABCMeta, type):
+    """
+    Singleton metaclass for ensuring only one instance of a class.
+    """
+
+    _instances = {}
+
+    def __call__(cls, *args, **kwargs):
+        """Call method for the singleton metaclass."""
+        if cls not in cls._instances:
+            cls._instances[cls] = super(Singleton, cls).__call__(*args, **kwargs)
+        return cls._instances[cls]
+
+
+class AbstractSingleton(abc.ABC, metaclass=Singleton):
+    pass
+
+
+class Config(metaclass=Singleton):
+    """
+    Configuration class to store the invokation settings of LLMind
+    """
+
+    def __init__(self):
+        self.openai_api_key = os.getenv("OPENAI_API_KEY", "None")
--- src/main.py
+++ src/main.py
@@ -1,5 +1,18 @@
+import openai
+from src.config import Config
+
+cfg = Config()
+
+
 def main():
-    print("Hello World")
+    if cfg.openai_api_key == "None":
+        print("OpenAI API key not found. Please set it in .env")
+        exit(1)
+    else:
+        openai.api_key = cfg.openai_api_key
+
+    print(openai.api_key)
+
 
 if __name__ == "__main__":
     main()
