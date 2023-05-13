54a554edb53a1148af974669aeac8bec03d2dad1
MysteryMooCows <rod.j.myers@gmail.com>
2023-05-13 17:32:19 -0400

    Revert "Broken working branch, added scratch.py and scratch.ipynb to /src and added initialize() to main"
    This reverts commit c6adb47b96cc620edce0cdc3356940201d338d46.

--- src/config.py
+++ src/config.py
@@ -32,5 +32,3 @@ class Config(metaclass=Singleton):
 
     def __init__(self):
         self.openai_api_key = os.getenv("OPENAI_API_KEY", "None")
-        self.model = os.getenv("MODEL", "None")
-        self.debug_mode = os.getenv("DEBUG_MODE", "False")
